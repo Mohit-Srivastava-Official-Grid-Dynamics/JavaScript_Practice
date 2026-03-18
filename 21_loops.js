1349c309e11fa2e9cbfaf55d2eee3c71b421dc0c
Mohit-Srivastava-Official-Grid-Dynamics <msrivastava@griddynamics.com>
2026-03-18 10:52:23 +0530

    Covered loops

---
+++ 21_loops.js
@@ -0,0 +1,56 @@
+//For
+
+for(let i = 0;i<=10;i++)
+{
+    const element = i;
+    if(element == 5)
+    {
+        // console.log("I am at 5");
+    }
+    // console.log(element);
+}
+// console.log(element);//out of scope
+
+
+// for(let i=1;i<=10;i++)
+// {
+//     console.log(`Outer loop value :i = ${i}`);
+//     for(let j=1;j<=10;j++)
+//         {
+//             console.log(`Inner loop value : i = ${i} and j= ${j}`);
+            // console.log(i + '*' + j + ' = ' + i*j );
+
+//     }
+// }
+
+let myArray = ["flash","Batman","Superman"];
+// console.log(myArray.length);
+for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];
+    // console.log(element);
+    
+}
+
+//Break and Continue
+
+for(let index = 1;index<=20;index++)
+{
+    if(index == 5)
+    {
+        console.log(`Detected 5`);
+        break;
+    }
+    // console.log(`Value of i is ${index}`);
+}
+
+
+for(let index = 1;index<=20;index++)
+    {
+        if(index==5)
+            {
+                console.log(`Detected 5`);
+                continue;
+            }
+            console.log(`Value of i is ${index}`);
+
+}
